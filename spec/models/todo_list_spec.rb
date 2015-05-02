d5bd20a159187d3c2f0f73ce90dbe11063891fe7
Yungchih Chen <yungchih@nclud.com>
2015-05-01 23:54:08 -0400

    Clean up Todo List and show pages

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe TodoList, :type => :model do
   it { should have_many(:todo_items)}
+
+  describe "#has_complete_items?" do
+  	let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list")}
+
+  	it "returns true with completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)	
+  		expect(todo_list.has_completed_items?).to be true
+  	end
+
+  	it "returns false with no completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs")
+  		expect(todo_list.has_completed_items?).to be false	
+  	end
+
+  end
+
+  describe "#has_incomplete_items?" do
+  	let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list")}
+
+  	it "returns true with incompleted todo list items" do
+  		todo_list.todo_items.create(content: "Eggs")
+  		expect(todo_list.has_incompleted_items?).to be true	
+  		
+  	end
+
+  	it "returns false with no incompleted todo list items" do
+			todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)	
+  		expect(todo_list.has_incompleted_items?).to be false
+  	end
+
+  end
 end
